8536bb9cf82ccbc024d9da99d911f9a873f6728a
Alexandre Neto <senhor.neto@gmail.com>
2019-12-31 02:31:02 +0000

    Fix requirements

--- venv.mk
+++ venv.mk
@@ -11,8 +11,8 @@ venv/venv.timestamp:
 
 # Install requirements and create a timestamp file
 # NOTE: we use --pre with pip install to have the not yet release 2.0 Sphinx
-venv/REQUIREMENTS.timestamp: venv/venv.timestamp REQUIREMENTS.txt
-	venv/bin/pip install --pre -r REQUIREMENTS.txt
+venv/requirements.timestamp: venv/venv.timestamp requirements.txt
+	venv/bin/pip install --pre -r requirements.txt
 	touch $@
 
 include Makefile
@@ -22,6 +22,6 @@ cleanall: springclean
 	rm -rf venv
 
 # Add dependencies to targets that require sphinx
-html: venv/REQUIREMENTS.timestamp
-gettext: venv/REQUIREMENTS.timestamp
-fasthtml: venv/REQUIREMENTS.timestamp
+html: venv/requirements.timestamp
+gettext: venv/requirements.timestamp
+fasthtml: venv/requirements.timestamp
